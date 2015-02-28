7033c38f26cfcedd2a3e793d03d2d93a6d6b453c
Benni <ben.athiwaratkun@gmail.com>
2015-02-27 23:13:02 -0500

    Cost Function Tested!

--- src/NeuralNet/NeuralNet.py
+++ src/NeuralNet/NeuralNet.py
@@ -11,6 +11,10 @@ import inspect
 import collections
 EPSILON = 0.12
 
+def sigmoid(z):
+    return 1.0/(1+np.exp(-z))
+def sigmoidGrad(z):
+    return sigmoid(z)*(1-sigmoid(z))
 
 class NeuralNet:
     ''' Required Fields '''
@@ -22,11 +26,6 @@ class NeuralNet:
     actFns = []
     actGs = []
     numLayers = 0
-
-    def sigmoid(self, z):
-        return 1.0/(1+np.exp(-z))
-    def sigmoidGrad(self, z):
-        return self.sigmoid(z)*(1-self.sigmoid(z))
 
     activationDicts = {}
     activationDicts['sigmoid'] = (sigmoid, sigmoidGrad)
@@ -42,21 +41,25 @@ class NeuralNet:
                                                     '../../data/digits/digits_designMatrix.txt') )
             data_labels = np.loadtxt(os.path.join( current_folder_path,
                                                    '../../data/digits/digits_label.txt'))
+            data_labels = np.vectorize(lambda (x): (x -1))(data_labels)
         else:
             print "Other Data"
             
         ''' Adding column of 1's to X'''
         self.numInputs, self.inputDim = np.shape(designMatrix)
-        designMatrix = np.c_[np.ones((self.numInputs,1)), designMatrix]
+        designMatrix = np.column_stack( (np.ones((self.numInputs)), designMatrix) )
+        label_matrix = np.zeros((self.numInputs, self.outputDim))
+        for i in range(self.numInputs):
+            label_matrix[i,data_labels[i]] = 1
         Data = collections.namedtuple('Data', ['X','Y'])
-        d = Data(designMatrix, data_labels)
+        d = Data(designMatrix, label_matrix)
         return d
             
     
     def __init__(self, trainingData='MNIST',
                  numClasses=10,
                  hiddenLayersSize=[25], 
-                 activationFunctions=['sigmoid']):
+                 activationFunctions=['sigmoid', 'sigmoid']):
         print 'Initializing Neural Network Object'
         self.outputDim = numClasses
         self.trainData = self.loadData(trainingData)
@@ -70,36 +73,46 @@ class NeuralNet:
             _Theta = np.random.uniform(-EPSILON,EPSILON,(hSizeNext, hSize+1))
             # randomize values
             self.Thetas.append(_Theta)
-            #print np.shape(self.Thetas[i])
         ## activation function
         print '2. Mapping Activation Functions', activationFunctions
         for a in activationFunctions:
             if a in self.activationDicts:
-                self.actFns, self.actGs = self.activationDicts[a]
+                _func, _grad = self.activationDicts[a]
+                self.actFns.append(_func)
+                self.actGs.append(_grad)
             else:
                 print 'Unsupported Activation Function:', a
                 print 'Dictionary of Supported Functions:', self.activationDicts
     
     def gradient(self, **args):
-        return self.gradientLogLoss(self, X=self.trainData[0], y=self.trainData[1], regParams=[1.0,1.0])
+        print type(self.trainData[0])
+        return self.gradientLogLoss(X=self.trainData[0], y=self.trainData[1], regParams=[1.0,1.0])
     
 
-    def gradientLogLoss(self, X,y, thetaList, regParams):
+    def gradientLogLoss(self, X, y, regParams):
         ## Initial w/o any hidden layers
-        H = - np.mat(X) * np.mat(thetaList[0].T)
-        H = np.vectorize(self.actFns[0])(H)
-
+        #H = np.mat(X) * np.mat(self.Thetas[0].T)
+        H = np.dot(X, self.Thetas[0].T)
+        H = self.actFns[0](H)
         ## Now for each hidden layer:
         for i in range(1, len(self.actFns)):
-            H = - np.mat(np.concatenate((np.ones((H.shape[0],1)), H), axis=1))  * np.mat(thetaList[i].T)
-            H = np.vectorize(self.actFns[i])(H)
+            print 'i=', i
+            H = np.dot(np.concatenate((np.ones((self.numInputs,1)), H), axis=1),
+                       self.Thetas[i].T )
+            H = self.actFns[i](H)
+        ##  Unregularized Cost Function  
+        J = np.sum(- np.multiply(y, np.log(H)) + np.multiply( y-1.0, np.log(1.0-H)) )/self.numInputs
 
-            ##  Unregularized Cost Function
-        J = np.sum(-np.multiply(y, np.log(H)) + np.multiply(1-y, np.log(1-H)))/X.shape[0]
+        print "Unregularized cost", J
 
         ## Regularized Cost Function
-        for i in range(len(thetaList)):
-            J += regParams[i]/(X.shape[0]) * np.sum(np.multiply(thetaList[i][:,1:],thetaList[i][:,1:]) )
+        for i in range(len(self.Thetas)):
+            J += regParams[i]/(2.0*X.shape[0]) * np.sum(np.square(self.Thetas[i][:,1:]))
+        
+        print "Regularized Cost", J
+        
+        print H[-1,:]
+        return [J,[1]]
         
     
     def train(self, tolerance=0.01, maxNumIts = 1000, regParams=[0.01]):
@@ -130,11 +143,25 @@ class NeuralNet:
         h = self.hypothesis(self, X)
         return  np.argmax(h,axis=1) # TODO - check if it's +1
 
+
+    def test_loadSampleThetas(self):
+        import os
+        import inspect
+        current_folder_path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+        Theta1 = np.loadtxt(os.path.join( current_folder_path,
+                                          '../../data/digits/digits_Theta1.txt'), delimiter=',')
+        Theta2 = np.loadtxt(os.path.join( current_folder_path, 
+                                          '../../data/digits/digits_Theta2.txt'), delimiter=',')
+        self.Thetas = [Theta1, Theta2]
+        
+        print self.gradient()
+
 def main():
     nn = NeuralNet()
     print [np.shape(ob) for ob in nn.Thetas]
-    nn.train()
+    #nn.train()
     print [np.shape(i) for i in nn.trainData]
+    nn.test_loadSampleThetas()
     
     
     
