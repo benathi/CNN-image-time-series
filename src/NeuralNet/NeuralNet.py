3b9ca3ab7727972e80d6a33550847b902210a9d3
Benni <ben.athiwaratkun@gmail.com>
2015-02-28 01:16:27 -0500

    Neural Net Tested - Works

--- src/NeuralNet/NeuralNet.py
+++ src/NeuralNet/NeuralNet.py
@@ -85,50 +85,71 @@ class NeuralNet:
                 print 'Dictionary of Supported Functions:', self.activationDicts
     
     def gradient(self, **args):
-        print type(self.trainData[0])
-        return self.gradientLogLoss(X=self.trainData[0], y=self.trainData[1], regParams=[1.0,1.0])
+        return self.gradientLogLoss(X=self.trainData[0], y=self.trainData[1],
+                                    regParams=args['regParams'])
     
 
-    def gradientLogLoss(self, X, y, regParams):
+    def gradientLogLoss(self, regParams, X, y):
+        ## 1. Cost Function
+        m = np.shape(X)[0]*1.0
         ## Initial w/o any hidden layers
-        #H = np.mat(X) * np.mat(self.Thetas[0].T)
         H = np.dot(X, self.Thetas[0].T)
         H = self.actFns[0](H)
         ## Now for each hidden layer:
         for i in range(1, len(self.actFns)):
-            print 'i=', i
-            H = np.dot(np.concatenate((np.ones((self.numInputs,1)), H), axis=1),
+            H = np.dot(np.concatenate((np.ones((m,1)), H), axis=1),
                        self.Thetas[i].T )
             H = self.actFns[i](H)
         ##  Unregularized Cost Function  
-        J = np.sum(- np.multiply(y, np.log(H)) + np.multiply( y-1.0, np.log(1.0-H)) )/self.numInputs
-
-        print "Unregularized cost", J
-
-        ## Regularized Cost Function
+        J = np.sum(- np.multiply(y, np.log(H)) + np.multiply( y-1.0, np.log(1.0-H)) )/m
+        #print "Unregularized cost", J
         for i in range(len(self.Thetas)):
             J += regParams[i]/(2.0*X.shape[0]) * np.sum(np.square(self.Thetas[i][:,1:]))
+        #print "Regularized Cost", J
         
-        print "Regularized Cost", J
+        ## 2. Gradient
+        ## First, calculate initial values:
+        ## zvec goes from z_2, z_3, ... 
+        zvec = [np.dot(X,self.Thetas[0].T)]
+        for i in range(1,len(self.Thetas)):
+            zvec.append(  np.dot( np.column_stack( (np.ones((m,1)) ,self.actFns[i-1](zvec[i-1])) ),
+                                         self.Thetas[i].T)   )
+        delta_vec = [ self.actFns[-1](zvec[-1]) - y]
+        for i in range(1, len(self.Thetas)):
+            j = len(self.Thetas) - i - 1
+            delta_vec.insert(0,
+                             np.dot(delta_vec[0], self.Thetas[j+1])[:,1:] * self.actGs[j+1](zvec[j]))
         
-        print H[-1,:]
-        return [J,[1]]
+        Theta_grads = [ (1.0/m) * np.dot(delta_vec[0].T, X )     ]
+        for i in range(len(self.Thetas)-1):
+            Theta_grads.append( (1.0/m)*np.dot( delta_vec[i+1].T,
+                                                np.column_stack( (np.ones(np.shape(zvec[i])[0]), self.actFns[i](zvec[i]))) )
+                                                )
+        for i in range(len(self.Thetas)-1):
+            Theta_grads[i] += (regParams[i]/m)*np.column_stack( ((np.zeros(np.shape(self.Thetas[i])[0])),
+                                                                self.Thetas[i][:,1:]))
+            #Theta_grads[j] += (regParams[i]/m)*self.Thetas[i]
+        
+        return (J,Theta_grads)
         
     
-    def train(self, tolerance=0.01, maxNumIts = 1000, regParams=[0.01]):
+    def train(self, tolerance=0.0001, maxNumIts = 10000, regParams=[1.0,1.0]):
         print 'Training'
-        numIt = 0
-        alpha = 0.01    ## TODO - configurable
+        numIt = -1
+        alpha = 0.3    ## TODO - configurable
         costList = []
-        while True:
+        while numIt < maxNumIts:
             numIt += 1
             J,G = self.gradient(regParams=regParams)
             for i in range(len(self.Thetas)):
-                self.Thetas[i] += alpha*G[i]
+                self.Thetas[i] -= alpha*G[i]
             costList.append(J)
             print 'Iteration %d: Cost = %f' % (numIt, J)
-            if (numIt > 30 and (costList[numIt-1]-J)/J < tolerance) or numIt > 1000:
-                break
+            if numIt > 30:
+                percentTage = (costList[numIt-1]-costList[numIt])/J*100.0
+                #print 'Decreased by %f percent' % percentTage
+                if percentTage < tolerance:
+                    break
     
     def hypothesis(self, X):
         h = np.array(X) # deep copy
@@ -154,14 +175,28 @@ class NeuralNet:
                                           '../../data/digits/digits_Theta2.txt'), delimiter=',')
         self.Thetas = [Theta1, Theta2]
         
-        print self.gradient()
+        nRow1, nCol1 = np.shape(Theta1)
+        nRow2, nCol2 = np.shape(Theta2)
+        DEL = 0.000001
+        diff_error = 0.0
+        for r in range(nRow1):
+            for c in range(nCol1):
+                print '(r,c)=(%d,%d)' % (r,c)
+                J_before, G_before = self.gradient(X=self.trainData[0], y=self.trainData[1])
+                self.Thetas[0][r,c] += DEL
+                J_after, G_after = self.gradient(X=self.trainData[0], y=self.trainData[1])
+                diff_error += ( (J_after-J_before)/DEL - G_before[0][r,c] )**2
+                print 'Accumulated Error = ', diff_error
+        
+        
 
 def main():
-    nn = NeuralNet()
+    nn = NeuralNet(hiddenLayersSize=[25, 15], 
+                 activationFunctions=['sigmoid', 'sigmoid', 'sigmoid'])
     print [np.shape(ob) for ob in nn.Thetas]
-    #nn.train()
+    nn.train(regParams=[0.1,0.1,0.1])
     print [np.shape(i) for i in nn.trainData]
-    nn.test_loadSampleThetas()
+    #nn.test_loadSampleThetas()
     
     
     
