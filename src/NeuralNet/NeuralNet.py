Gradient Fixed - Tested

src/NeuralNet/NeuralNet.py
@@ -139,7 +139,7 @@ class NeuralNet:
             Theta_grads.append( (1.0/m)*np.dot( delta_vec[i+1].T,
                                                 np.column_stack( (np.ones(np.shape(zvec[i])[0]), self.actFns[i](zvec[i]))) )
                                                 )
-        for i in range(len(Ts)-1):
+        for i in range(len(Ts)):
             Theta_grads[i] += (regParams[i]/m)*np.column_stack( ((np.zeros(np.shape(Ts[i])[0])),
                                                                 Ts[i][:,1:]))
         return np.array(Theta_grads)
@@ -188,7 +188,6 @@ class NeuralNet:
     
     def packThetas(self, T_list):
         #return np.concatenate(  (np.ndarray.flatten(Theta1), np.ndarray.flatten(Theta2)) , axis = 0)
-
         return np.concatenate( [np.ndarray.flatten(Ti) for Ti in T_list] , axis=0)
     
     def regGradientWithCost_1D(self, T1D, X, y, regParams):
@@ -206,9 +205,9 @@ class NeuralNet:
         ''' TODO - see if packing and unpacking gives us the same Thetas'''
         X = self.trainData[0]
         y = self.trainData[1]
-        J, Thetas_expanded = fmincg(lambda(T1D) : self.regGradientWithCost_1D(T1D, X, y, regParams),
+        Thetas_expanded, fX = fmincg(lambda(T1D) : self.regGradientWithCost_1D(T1D, X, y, regParams),
                                          Thetas_expanded, MaxIter=200)
-        self.Thetas = self.packThetas(Thetas_expanded)
+        self.Thetas = self.unpackTheta(Thetas_expanded)
         
         print self.ReportLogLossScore()
     
@@ -253,33 +252,47 @@ class NeuralNet:
         Theta1, Theta2 = self.Thetas
         nRow1, nCol1 = np.shape(Theta1)
         nRow2, nCol2 = np.shape(Theta2)
-        regParams = [4.0]*2
-        DEL = 0.000001
+        regParams = [100000.0]*2
+        DEL = 0.000000001
         diff_error = 0.0
-#        for r in range(nRow1):
-#            for c in range(nCol1):
-#                print '(r,c)=(%d,%d)' % (r,c)
-#                J_before, G_before = self.regGradientWithCost(self.Thetas, X=self.trainData[0], y=self.trainData[1], regParams=regParams)
-#                self.Thetas[0][r,c] += DEL
-#                J_after, G_after = self.regGradientWithCost(self.Thetas, X=self.trainData[0], y=self.trainData[1], regParams=regParams)
-#                #G_before[0][r,c] += (J_after-J_before)/DEL
-#                diff_error += np.linalg.norm(G_before[0]-G_after[0]) + np.linalg.norm(G_before[1]-G_after[1])
-#                print G_before[0][r,c]
-#                print G_after[0][r,c]
-#                print (J_after-J_before)/DEL
-#                print 'Accumulated Error in theta 1 = ', diff_error
+        finiteDiffs_grad1 = np.zeros_like(Theta1)
+        
+        Grad = self.regGradient(self.Thetas, X=self.trainData[0], y=self.trainData[1], regParams=regParams)
+        Grad1 = Grad[0]
+        Grad2 = Grad[1]
+        
+        for r in range(nRow1):
+            for c in range(nCol1):
+                print '(r,c)=(%d,%d)' % (r,c)
+                J_before = self.regCost(self.Thetas, X=self.trainData[0], y=self.trainData[1], regParams=regParams)
+                self.Thetas[0][r,c] += DEL
+                J_after = self.regCost(self.Thetas, X=self.trainData[0], y=self.trainData[1], regParams=regParams)
+                #G_before[0][r,c] += (J_after-J_before)/DEL
+                #diff_error += np.linalg.norm(G_before[0]-G_after[0]) + np.linalg.norm(G_before[1]-G_after[1])
+                #print G_before[0][r,c]
+                #print G_after[0][r,c]
+                finiteDiffs_grad1[r,c] = (J_after-J_before)/DEL
+                #print 'Accumulated Error in theta 1 = ', diff_error
+        finiteDiffs_grad2 = np.zeros_like(Theta2)
         for r in range(nRow2):
             for c in range(nCol2):
                 print '(r,c)=(%d,%d)' % (r,c)
-                J_before, G_before = self.regGradientWithCost(self.Thetas, X=self.trainData[0], y=self.trainData[1], regParams=regParams)
+                J_before = self.regCost(self.Thetas, X=self.trainData[0], y=self.trainData[1], regParams=regParams)
                 self.Thetas[1][r,c] += DEL
-                J_after, G_after = self.regGradientWithCost(self.Thetas, X=self.trainData[0], y=self.trainData[1], regParams=regParams)
+                J_after = self.regCost(self.Thetas, X=self.trainData[0], y=self.trainData[1], regParams=regParams)
                 #G_before[0][r,c] += (J_after-J_before)/DEL
-                diff_error += np.linalg.norm(G_before[0]-G_after[0]) + np.linalg.norm(G_before[1]-G_after[1])
-                print G_before[1][r,c]
-                print G_after[1][r,c]
-                print (J_after-J_before)/DEL
-                print 'Accumulated Error in theta 2= ', diff_error
+                #diff_error += np.linalg.norm(G_before[0]-G_after[0]) + np.linalg.norm(G_before[1]-G_after[1])
+                #print G_before[1][r,c]
+                #print G_after[1][r,c]
+                finiteDiffs_grad2[r,c] =  (J_after-J_before)/DEL
+                #print 'Accumulated Error in theta 2= ', diff_error
+        print Grad2 - finiteDiffs_grad2
+        print Grad1 - finiteDiffs_grad1
+        
+        print 'Difference in Grad1', np.linalg.norm(Grad1 - finiteDiffs_grad1)
+        print 'Difference in Grad2', np.linalg.norm(Grad2 - finiteDiffs_grad2)
+        print np.max(np.abs(Grad1 - finiteDiffs_grad1))
+        print np.max(np.abs(Grad2 - finiteDiffs_grad2))
         
         
 def testNeuralNet():
@@ -292,11 +305,11 @@ def testNeuralNet():
 
 def main():
     nn = NeuralNet(trainingData = 'DIGITS',
-                   hiddenLayersSize=[200]*2, 
+                   hiddenLayersSize=[100]*2, 
                  activationFunctions=['sigmoid']*3)
     print [np.shape(ob) for ob in nn.Thetas]
-    #nn.train(maxNumIts=10000,regParams=[0.1]*3)
-    nn.train_cg(regParams=[0.1]*3)
+    nn.train(maxNumIts=10000,regParams=[0.1]*3)
+    #nn.train_cg(regParams=[0.1]*3)
     print [np.shape(i) for i in nn.trainData]
     #nn.test_loadSampleThetas()
     #print(nn.trainData[1] )
@@ -305,5 +318,5 @@ def main():
 
     
 if __name__ == "__main__":
-    testNeuralNet()
-    #main()
+    #testNeuralNet()
+    main()

src/NeuralNet/fmincg.py
@@ -55,6 +55,7 @@ from test.test_userdict import d2
 
 def fmincg(f, X, **args):
     Thetas = np.copy(X)
+    print 'Shape of Thetas', np.shape(Thetas)
     length = 100
     if 'MaxIter' in args:
         length = args['MaxIter']
@@ -73,16 +74,16 @@ def fmincg(f, X, **args):
     fX = []
     f1, df1 = f(Thetas)
     i += (length<0)
-    s = -df1
+    s = -np.copy(df1)
     d1 = -np.dot(s.T, s)
     z1 = red/(1.0-d1)
     while i < abs(length):
         i += (length > 0)
         print 'Iteration ', i
-        Thetas0 = Thetas
+        Thetas0 = np.copy(Thetas)
         f0 = f1
-        df0 = df1
-        Thetas = Thetas+ np.dot(z1,s)
+        df0 = np.copy(df1)
+        Thetas += np.dot(z1,s)
         f2, df2 = f(Thetas)
         i += (length < 0)
         d2 = np.dot(df2.T,s)
@@ -97,31 +98,32 @@ def fmincg(f, X, **args):
         limit = -1
         while 1:
             #print "check while loop"
-            while (( f2> f1 + RHO * np.dot(z1,d1)) or (d2 > -SIG * d1) and (M>0) ):
+            while  ( (f2> f1 + RHO * np.dot(z1,d1)) or (d2 > -SIG * d1)) and (M>0):
                 limit = z1
                 if (f2 > f1):
                     z2 = z3 - (0.5 * np.dot(d3, np.dot(z3,z3)))/(np.dot(d3,z3) + f2 - f3) 
                 else:
-                    A = 6*(f2-f3)/z3 + 3 *(d2+d3)
-                    B = 3*(f3-f2) - z3*(d3 + 2*d2)
-                    z2 = (math.sqrt(B*B - np.dot(np.dot(A * d2,z3),z3)) - B)/A
+                    A = 6.0*(f2-f3)/z3 + 3.0*(d2+d3)
+                    B = 3.0*(f3-f2) - z3*(d3 + 2.0*d2)
+                    z2 = (math.sqrt(B*B - A * d2*z3*z3) - B)/A
                 if math.isnan(z2):
-                    z2 = z3/2
+                    z2 = z3/2.0
                 z2 = max(min(z2, INT*z3), (1.0-INT)*z3)
                 z1 = z1+z2
-                Thetas = Thetas + np.dot(z2,s)
+                Thetas += np.dot(z2,s)
                 f2, df2 = f(Thetas)
-                M = M -1
+                M -= 1
                 i += (length<0)
                 d2 = np.dot(df2.T,s)
-                z3 = z3-z2
+                z3 -= z2
+                print M
             #print 'Could break out of loop'
             #print f2, "f2"
             #print d2, "d2"
             #print M, "M"
             #print d2
             #print (SIG*d1)
-            if (f2 > f1 + RHO * np.dot(z1,d1)) or (d2 > -SIG*d1):
+            if (f2 > f1 + RHO * z1*d1) or (d2 > -SIG*d1):
                 break
             elif d2 > SIG * d1:
                 success = 1
@@ -129,16 +131,16 @@ def fmincg(f, X, **args):
             elif M == 0:
                 break
             #print 'Didnt break'
-            A = 6*(f2-f3) /z3  + 3*(d2+d3)
-            B = 3*(f3-f2) - z3*(d3 + 2*d2)
-            z2 = -np.dot(np.dot(d2,z3),z3) / ( B + math.sqrt( B*B - np.dot(np.dot(A*d2,z3),z3)))
+            A = 6.0*(f2-f3)/(1.0*z3)  + 3.0*(d2+d3)
+            B = 3.0*(f3-f2) - z3*(d3 + 2.0*d2)
+            z2 = -d2*z3*z3 / ( B + math.sqrt( B*B - A*d2*z3*z3))
             if (math.isnan(z2)) or isinstance(z2, complex) or z2 < 0:
                 if limit < -0.5:
                     z2 = z1 * (EXT-1.0)
                 else:
-                    z2 = (limit - z1)/2
+                    z2 = (limit - z1)/2.0
             elif (limit > -0.5) and (z2+z1 > limit):
-                z2 = (limit-z1)/2
+                z2 = (limit-z1)/2.0
             elif (limit < -0.5) and (z2 +z1 > z1 * EXT):
                 z2 = z1*(EXT-1.0)
             elif z2 < -z3*INT:
@@ -149,40 +151,47 @@ def fmincg(f, X, **args):
             d3 = d2
             z3 = -z2
             z1 = z1+z2
-            Thetas = Thetas + np.dot(z2,s)
+            Thetas += np.dot(z2,s)
             f2, df2 = f(Thetas)
-            M = M-1
+            M -= 1
             i += (length<0)
             d2 = np.dot(df2.T,s)
             
             #print i 
+        print 'Outside while 1'
         if success:
+            print 'success'
             f1 = f2
-            fX = [fX.T, f1].T
+            #fX = [fX.T, f1].T
+            fX = np.concatenate((fX, [f1]))
             s = (np.dot(df2.T , df2) - np.dot(df1.T,df2))/ (np.dot(np.dot(df1.T,df1),s) ) - df2
-            tmp = df1
-            df1 = df2
-            df2 = tmp
+            tmp = np.copy(df1)
+            df1 = np.copy(df2)
+            df2 = np.copy(tmp)
             d2 = np.dot(df1.T,s)
             if d2 > 0:
-                s = -df1
+                s = -np.copy(df1)
                 d2 = -np.dot(s.T,s)
             z1 = z1 * min(RATIO, d1/(d2-sys.float_info.min))
             d1 = d2
             ls_failed = 0
         else:
-            Thetas = Thetas0
+            Thetas = np.copy(Thetas0)
             f1 = f0
-            df1 = df0
+            df1 = np.copy(df0)
             if ls_failed or (i > abs(length)):
+                print 'Line Search Failed'
                 break
-            tmp = df1
-            df1 = df2
-            df2 = tmp
-            s = -df1
+            tmp = np.copy(df1)
+            df1 = np.copy(df2)
+            df2 = np.copy(tmp)
+            s = -np.copy(df1)
             d1 = -np.dot(s.T,s)
             z1 = 1/(1-d1)
             ls_failed = 1
+    
+    print 'fmincg Return'
+    print Thetas
     return (Thetas, fX)
 
 