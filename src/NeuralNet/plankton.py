fa157e01932cbaf89dd8be0a39b34163359fa348
Benni <ben.athiwaratkun@gmail.com>
2015-03-12 01:30:51 -0400

    Deleted imports

--- src/NeuralNet/plankton.py
+++ src/NeuralNet/plankton.py
@@ -8,23 +8,11 @@ Created on Mar 6, 2015
 #Import libraries for doing image analysis
 from skimage.io import imread
 from skimage.transform import resize
-from sklearn.ensemble import RandomForestClassifier as RF
 import glob
 import os
-from sklearn import cross_validation
-from sklearn.cross_validation import StratifiedKFold as KFold
-from sklearn.metrics import classification_report
-from matplotlib import pyplot as plt
-from matplotlib import colors
-from pylab import cm
-from skimage import segmentation
-from skimage.morphology import watershed
-from skimage import measure
-from skimage import morphology
 import numpy as np
-import pandas as pd
-from scipy import ndimage
-from skimage.feature import peak_local_max
+#import pandas as pd
+#from scipy import ndimage
 import warnings
 warnings.filterwarnings("ignore")
 import inspect
