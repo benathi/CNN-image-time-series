451ba762c79576e0b68ba85b6135b5810263ebd5
Benni <ben.athiwaratkun@gmail.com>
2015-05-10 19:54:47 -0400

    fix important activations

--- src/pylearn2_plankton/ImportantActivations.py
+++ src/pylearn2_plankton/ImportantActivations.py
@@ -228,9 +228,10 @@ def rfOnActivationsPerformance(model_name, data_spec, which_layer, maxPixel):
     print 'which_layer (max layer)', which_layer
     print 'maxPixel', maxPixel
     print 'Obtaining Activations'
-    X_train, Y_train, X_test, Y_test= prepXY(model_name, data_spec, which_layer, maxPixel)
+    
     for layer in range(which_layer):
         print '**************************************************************'
+        X_train, Y_train, X_test, Y_test= prepXY(model_name, data_spec, layer, maxPixel)
         print 'layer =', layer
         treeImportance(X_train, Y_train, X_test, Y_test, model_name, layer)
     
