07eb4def07e43ef9b99a4946968763fb9d51517f
benathi <ben.athiwaratkun@gmail.com>
2015-05-11 01:43:51 +0000

    Modify Bagging CNN

--- src/pylearn2_plankton/BaggingCNN.py
+++ src/pylearn2_plankton/BaggingCNN.py
@@ -64,7 +64,7 @@ def prepXY(model_name, data_spec, which_layer, maxPixel):
     # combine CV to test
     #rawX_train = np.concatenate((rawX_train, rawX_cv), axis=0)
     #Y_train = np.concatenate((Y_train, Y_cv), axis=0)
-    X_test = findActivations(model_name, [rawX_test], which_layer, maxPixel)
+    X_test = findActivations(model_name, [rawX_test], which_layer, maxPixel)[0]
     # 2. find activations
     print 'Done Finding Activations'
     return (X_test, Y_test)
@@ -89,6 +89,7 @@ def rfOnActivationsPerformance(model_name, data_spec, which_layer, maxPixel):
     for i in range(5):
         model_name_bag = model_name + '_bag'+ str(i) + '.pkl'
         X_test, Y_test= prepXY(model_name_bag, data_spec, -1, maxPixel)
+        print 'shape of X_test', X_test.shape
         if X_test_acc is None:
             X_test_acc = X_test
         else:
